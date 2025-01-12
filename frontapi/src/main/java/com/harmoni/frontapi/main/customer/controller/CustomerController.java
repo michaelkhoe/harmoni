a4d2e48694a5693b07a6e8b8fee05a3ca8e583a0
vembry <airstrip99@gmail.com>
2025-01-12 08:04:18 +0000

    chore: update api specs

--- frontapi/src/main/java/com/harmoni/frontapi/main/customer/controller/CustomerController.java
+++ frontapi/src/main/java/com/harmoni/frontapi/main/customer/controller/CustomerController.java
@@ -2,12 +2,18 @@ package com.harmoni.frontapi.main.customer.controller;
 
 import com.harmoni.frontapi.main.customer.service.CustomerService;
 import com.harmoni.frontapi.main.customer.service.model.Customer;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Customer APIs", description = "This a group of apis handling customer")
 @RestController
+@RequestMapping(path = "/api/v1/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -17,31 +23,35 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @PostMapping("/api/v1/addCustomer")
+    @PostMapping
+    @Operation(summary = "adds customer")
     public int addCustomer(@RequestBody Customer customer) {
         return customerService.addCustomer(customer);
     }
 
-    @GetMapping("/api/v1/getCustomerById")
-    public Customer getCustomerById(@RequestParam String id) {
+    @GetMapping("/{id}")
+    @Operation(summary = "gets customer by id", description = "gets customer by id passed on the path variable")
+    public Customer getCustomerById(@PathVariable String id) {
         return customerService.getCustomerById(id);
     }
 
-    @GetMapping("/api/v1/getAllCustomers")
+    @GetMapping("/all")
+    @Operation(summary = "gets all customers")
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
-    @PutMapping("/api/v1/updateCustomerById")
+    @PutMapping("/{id}")
+    @Operation(summary = "updates a customer by id", description = "updates a customer by id passed on the path variable")
     public int updateCustomerById(
-            @RequestParam String id,
-            @RequestBody Customer customer
-    ) {
+            @PathVariable String id,
+            @RequestBody Customer customer) {
         return customerService.updateCustomerById(id, customer);
     }
 
-    @DeleteMapping("/api/v1/deleteCustomerById")
-    public int deleteCustomerById(@RequestParam String id) {
+    @DeleteMapping("/{id}")
+    @Operation(summary = "deletes a customer by id", description = "deletes a customer by id passed on the path variable")
+    public int deleteCustomerById(@PathVariable String id) {
         return customerService.deleteCustomerById(id);
     }
 }
