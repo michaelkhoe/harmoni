90da66583a206d373ef5413e87ff50bbfb8aac57
vembry <airstrip99@gmail.com>
2025-01-15 17:56:45 +0000

    add refresh-tokens table

--- .docker/postgres/migration.sql
+++ .docker/postgres/migration.sql
@@ -21,3 +21,10 @@ CREATE TABLE public.sessions(
     created_at TIMESTAMP,
     expired_at TIMESTAMP
 );
+
+CREATE TABLE public.refresh_tokens(
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    session_id UUID REFERENCES sessions (id),
+    created_at TIMESTAMP,
+    expired_at TIMESTAMP
+);
