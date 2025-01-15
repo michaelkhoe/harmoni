0dfd1fcba98c0c3178fbf1b79cadc8d76e733f04
vembry <airstrip99@gmail.com>
2025-01-15 17:54:08 +0000

    add refresh handler

--- frontapi/src/main/java/com/harmoni/frontapi/main/user/controller/SessionController.java
+++ frontapi/src/main/java/com/harmoni/frontapi/main/user/controller/SessionController.java
@@ -1,6 +1,7 @@
 package com.harmoni.frontapi.main.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class SessionController {
     @GetMapping()
     @Operation(summary = "retrieve session data")
     public Object getSession() {
+        return null;
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "refresh session and generate new sesion access-token and refresh-token")
+    public Object refreshSession() {
         return null;
     }
 }
