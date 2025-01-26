7337aa9b3a0f758bcc01cb983bc491f97b04be51
lawrenceryan <52479198+lawrenceryan@users.noreply.github.com>
2025-01-26 14:02:12 +0700

    re-init cms template (#14)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router';
 
-createRoot(document.getElementById('root')!).render(
+import App from './app';
+import { routesSection } from './routes/sections';
+import { ErrorBoundary } from './routes/components';
+
+// ----------------------------------------------------------------------
+
+const router = createBrowserRouter([
+  {
+    Component: () => (
+      <App>
+        <Outlet />
+      </App>
+    ),
+    errorElement: <ErrorBoundary />,
+    children: routesSection,
+  },
+]);
+
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>
+);
